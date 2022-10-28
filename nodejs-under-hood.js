1940b13e7a4283369ee9b87503cc81a0c369ccdb
Ivan Grekov <ivan.grekov@windmill.digital>
2022-10-28 21:17:39 +0300

    microtasks

--- nodejs-under-hood.js
+++ nodejs-under-hood.js
@@ -1,35 +1,57 @@
-// JS engine V8
+// NOTE: JS engine V8
 
-// Call stack: [ main filter slice ]: slice -> filter -> main
+// NOTE: Call stack: [ main filter slice ]: slice -> filter -> main
 
-// Event loop: with help of `libuv` library
+// NOTE: Event loop: with help of `libuv` library
 
-// Task queue: [ f1 f2 f3 ]: f1 -> f2 -> f3
+// NOTE: Task queue: [ f1 f2 f3 ]: f1 -> f2 -> f3
 
 
-// -------------------------------------------------
+// ------------------------------------------------
 
-// Five queues in NodeJS:
 
-    // 1. Immediate callbacks: [ i1 i2 i3 ]
-    // setImmediate(() => {})
+// NOTE: Queues of Microtasks in NodeJS (Task queue takes one by one):
+
+    // 1. Next tick queue: [ nt1 ]
+    process.nextTick(() => {})
+
+    // 2. Promises and queueMicrotasks: [ pr1 ]
+    promise.then(() => {})
+    promise.catch(() => {})
+    queueMicrotasks(() => {})
+
+
+// ------------------------------------------------
+
+
+// Until we not empty `Next tick queue`, we can't start taking callbacks from `Promises and queueMicrotasks`
+
+// Until we not empty `Promises and queueMicrotasks`, we can't start taking callbacks from `Next tick queue`
+
+
+// ------------------------------------------------
+
+
+// NOTE: Five queues of Macrotasks in NodeJS (Task queue takes all at once):
+
+    // 1. Immediate callbacks: [ im1 im2 im3 ] 
+    setImmediate(() => {})
 
     // 2. Timer queue (timers, intervals): [ t1 t2 t3 ]
-    // setTimeout(() => {}, 0)
-    // setInterval(() => {}, 10)
+    setTimeout(() => {}, 0)
+    setInterval(() => {}, 10)
 
-    // 3. Pending callbacks (error handlers / network handlers): [ p1 p2 p3 ]
-    // server.on('connection', () => {})
+    // 3. Pending callbacks (error handlers / network handlers): [ pe1 pe2 pe3 ]
+    server.on('connection', () => {})
 
     // 4. Input / Output operations (most of async callback: readFile, server response ...): [ io1 io2 io3 ]
-    // fs.readFile('app.js', 'urf-8', () => {})
+    fs.readFile('app.js', 'urf-8', () => {})
 
-    // 5. Close callbacks (closing socket, close event of request): [ c1 c2 c3 ]
-    // req.on('close', () => {})
-
+    // 5. Close callbacks (closing socket, close event of request): [ cl1 cl2 cl3 ]
+    req.on('close', () => {})
 
     
-// -------------------------------------------------
+// ------------------------------------------------
 
 // 1. Pending callbacks: Immediate callbacks -> Timer queue -> Immediate callbacks -> Timer queue -> Pending callbacks
 
