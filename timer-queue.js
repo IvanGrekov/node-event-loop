e5a9dfc0f960476c044c2f1c5edcbb0ae50714c8
Ivan Grekov <ivan.grekov@windmill.digital>
2022-11-20 14:52:08 +0200

    additional checks - timer queue

--- timer-queue.js
+++ timer-queue.js
@@ -2,25 +2,70 @@ import fs from 'fs';
 
 console.clear();
 
-console.log('---- START ----');
+//#region NOTE: simple example
+// console.log('--Start--');
 
-fs.readFile('./index.html', 'utf-8', () => {
-    console.log('File');
-});
+// fs.readFile('README.md', (err, data) => {
+//     if (!err) {
+//         console.log('ReadFile');
 
-setTimeout(() => {
-    console.log('Timeout');
-}, 0);
+//         return;
+//     }
+// });
 
-setTimeout(() => {
-    console.log('Timeout 2 seconds');
-}, 2);
+// setTimeout(() => {
+//     console.log('Timeout');
+// }, 0);
 
-console.time('Loop');
-let count = 0;
-for (let index = 0; index < 10000000; index++) {
-    count = index;
-}
-console.timeEnd('Loop');
+// console.log('--End--');
 
-console.log('---- FINISH ----');
+// NOTE: callstack: [Start, End]
+// NOTE: callback queue: [Timeout, ReadFile]
+//#endregion
+
+//#region NOTE: example with big timeout delay
+// console.log('--Start--');
+
+// fs.readFile('README.md', (err, data) => {
+//     if (!err) {
+//         console.log('ReadFile');
+
+//         return;
+//     }
+// });
+
+// setTimeout(() => {
+//     console.log('Timeout');
+// }, 5);
+
+// console.log('--End--');
+
+// NOTE: callstack: [Start, End]
+// NOTE: callback queue: [ReadFile, Timeout]
+//#endregion
+
+//#region NOTE: Timeout and ReadFile in the callback queue at the same time
+// console.log('--Start--');
+
+// fs.readFile('README.md', (err, data) => {
+//     if (!err) {
+//         console.log('ReadFile');
+
+//         return;
+//     }
+// });
+
+// setTimeout(() => {
+//     console.log('Timeout');
+// }, 5);
+
+// for (let index = 0; index < 10000000; index++) {
+//     // NOTE: the loop takes more than 6ms
+//     index;
+// }
+
+// console.log('--End--');
+
+// NOTE: callstack: [Start, End]
+// NOTE: callback queue: [Timeout, ReadFile] NOTE: Timeout is the first since has a high-priority in the NodeJS event loop
+//#endregion
