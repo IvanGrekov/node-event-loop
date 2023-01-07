dcac05891f6f7c81cdd2ef7b8c7ac9d2dcb48773
Ivan Grekov <ivan.grekov@windmill.digital>
2023-01-07 19:17:16 +0200

    event-loop checks

--- timer-queue.js
+++ timer-queue.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
+import { nextTick } from 'process';
 
-console.clear();
+console.log('------');
 
 //#region NOTE: simple example
 // console.log('--Start--');
@@ -45,26 +46,46 @@ console.clear();
 //#endregion
 
 //#region NOTE: Timeout and ReadFile in the callback queue at the same time
-// console.log('--Start--');
+console.log('--Start--');
 
-// fs.readFile('README.md', (err, data) => {
-//     if (!err) {
-//         console.log('ReadFile');
+fs.readFile('README.md', (err, data) => {
+    if (!err) {
+        console.log('ReadFile');
 
-//         return;
-//     }
-// });
+        return;
+    }
+});
 
-// setTimeout(() => {
-//     console.log('Timeout');
-// }, 5);
+setTimeout(() => {
+    console.log('Timeout');
+}, 5);
 
-// for (let index = 0; index < 10000000; index++) {
-//     // NOTE: the loop takes more than 6ms
-//     index;
-// }
+// console.time('Loop');
+for (let index = 0; index < 10000000; index++) {
+    // NOTE: the loop takes more than 6ms
+    index;
+    if (index === 1999999) {
+        nextTick(() => {
+            console.log('nextTick');
+        });
+    }
+    if (index === 1999999) {
+        Promise.resolve('promise.resolve').then(console.log);
+    }
+    if (index === 1999999) {
+        queueMicrotask(() => {
+            console.log('queueMicrotask');
+        });
+    }
+    if (index === 1999999) {
+        setImmediate(() => {
+            console.log('setImmediate');
+        });
+    }
+}
+// console.timeEnd('Loop');
 
-// console.log('--End--');
+console.log('--End--');
 
 // NOTE: callstack: [Start, End]
 // NOTE: callback queue: [Timeout, ReadFile] NOTE: Timeout is the first since has a high-priority in the NodeJS event loop
